9d0483e9b26c8cd02823aff41582b2edff4c042d
mridha-rakib <rakibmahmudmridha@gmail.com>
2025-07-11 23:30:46 +0600

    Initial commit

---
+++ src/env.ts
@@ -0,0 +1,22 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+const env = createEnv({
+  server: {
+    MONGO_URI: z.string().url(),
+  },
+  shared: {
+    NODE_ENV: z
+      .enum(["development", "production", "test"])
+      .default("development"),
+    PORT: z.coerce.number().default(8000),
+    BASE_PATH: z.string().default("/api"),
+    LOG_LEVEL: z
+      .enum(["fatal", "error", "warn", "info", "debug", "trace", "silent"])
+      .default("info"),
+  },
+  // eslint-disable-next-line node/no-process-env
+  runtimeEnv: process.env,
+});
+
+export default env;
---
+++ src/middlewares/pino-logger.ts
@@ -0,0 +1,35 @@
+import env from "../env";
+import dayjs from "dayjs";
+import { randomUUID } from "node:crypto";
+import pino from "pino";
+import pinoHttp from "pino-http";
+
+const logger = pino({
+  level: env.LOG_LEVEL || "info",
+  timestamp: () => `"time":"${dayjs().format("YYYY-MM-DD HH:mm:ss")}"`,
+  ...(env.NODE_ENV !== "production" && {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        translateTime: "yyyy-mm-dd HH:MM:ss",
+      },
+    },
+  }),
+});
+
+function pinoLogger() {
+  return pinoHttp({
+    logger: logger as any,
+    genReqId(req, res) {
+      const existingID = req.id ?? req.headers?.["x-request-id"];
+      if (existingID) return existingID;
+      const id = randomUUID();
+      res.setHeader("X-Request-Id", id);
+      return id;
+    },
+    useLevel: env.LOG_LEVEL || "info",
+  });
+}
+
+export { logger, pinoLogger };
