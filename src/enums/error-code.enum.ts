5b9fe8ded5238077c39fd6abaa912512c02f6c3e
mridha-rakib <rakibmahmudmridha@gmail.com>
2025-07-14 12:51:31 +0600

    update error handler

---
+++ src/enums/error-code.enum.ts
@@ -0,0 +1,8 @@
+export const ErrorCodeEnum = {
+  CONFLICT_ERROR: 'CONFLICT_ERROR',
+  VALIDATION_ERROR: 'VALIDATION_ERROR',
+  RESOURCE_NOT_FOUND: 'RESOURCE_NOT_FOUND',
+  INTERNAL_SERVER_ERROR: 'INTERNAL_SERVER_ERROR',
+} as const;
+
+export type ErrorCodeEnumType = keyof typeof ErrorCodeEnum;
--- src/utils/error-handler.utils.ts
+++ src/utils/error-handler.utils.ts
@@ -1,55 +1,56 @@
-import { EHttpStatus } from '@/enums/http-status.enum';
+import { HTTPSTATUS, HttpStatusCodeType } from '@/config/http.config';
+import { ErrorCodeEnum, ErrorCodeEnumType } from '@/enums/error-code.enum';
 
 export class AppError extends Error {
-  public status: number;
-  public isOperational: boolean;
+  public statusCode: HttpStatusCodeType;
+  public errorCode?: ErrorCodeEnumType;
 
   constructor(
     message: string,
-    status: EHttpStatus = EHttpStatus.INTERNAL_SERVER_ERROR,
-    isOperational = true
+    statusCode = HTTPSTATUS.INTERNAL_SERVER_ERROR,
+    errorCode?: ErrorCodeEnumType
   ) {
     super(message);
-    this.status = status;
-    this.isOperational = isOperational;
-
+    this.statusCode = statusCode;
+    this.errorCode = errorCode;
     Error.captureStackTrace(this, this.constructor);
   }
 }
 
-export const handleError = (
-  message: string,
-  status: EHttpStatus = EHttpStatus.INTERNAL_SERVER_ERROR
-) => {
-  throw new AppError(message, status);
-};
-
-export class ValidationError extends AppError {
-  constructor(message: string) {
-    super(message, EHttpStatus.BAD_REQUEST);
+export class HttpException extends AppError {
+  constructor(
+    message = 'Http Exception Error',
+    statusCode: HttpStatusCodeType,
+    errorCode?: ErrorCodeEnumType
+  ) {
+    super(message, statusCode, errorCode);
   }
 }
 
-export class NotFoundError extends AppError {
-  constructor(message: string = 'Resource not found') {
-    super(message, EHttpStatus.NOT_FOUND);
+export class InternalServerException extends AppError {
+  constructor(message = 'Internal Server Error', errorCode?: ErrorCodeEnumType) {
+    super(
+      message,
+      HTTPSTATUS.INTERNAL_SERVER_ERROR,
+      errorCode || ErrorCodeEnum.INTERNAL_SERVER_ERROR
+    );
   }
 }
 
-export class UnauthorizedError extends AppError {
-  constructor(message: string = 'Unauthorized access') {
-    super(message, EHttpStatus.UNAUTHORIZED);
+export class NotFoundException extends AppError {
+  constructor(message = 'Resource not found', errorCode?: ErrorCodeEnumType) {
+    super(message, HTTPSTATUS.NOT_FOUND, errorCode || ErrorCodeEnum.RESOURCE_NOT_FOUND);
   }
 }
 
-export class ForbiddenError extends AppError {
-  constructor(message: string = 'Forbidden access') {
-    super(message, EHttpStatus.FORBIDDEN);
+export class BadRequestException extends AppError {
+  constructor(message = 'Bad Request', errorCode?: ErrorCodeEnumType) {
+    super(message, HTTPSTATUS.BAD_REQUEST, errorCode || ErrorCodeEnum.VALIDATION_ERROR);
   }
 }
 
 export class ConflictError extends AppError {
-  constructor(message: string = 'Resource conflict') {
-    super(message, EHttpStatus.CONFLICT);
+  constructor(message: string = 'Resource conflict', errorCode?: ErrorCodeEnumType) {
+    super(message, HTTPSTATUS.CONFLICT, errorCode || ErrorCodeEnum.CONFLICT_ERROR);
   }
 }
